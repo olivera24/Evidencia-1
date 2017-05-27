bc32fd92f16cacbd712336ec3e5f9dabd616f92a
olivera24 <dashatoledo78@gmail.com>
2017-05-26 19:41:17 -0500

    subiendo archivos

---
+++ Lab1/Lab1/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab1
+{
+    [Activity(Label = "Lab1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        
+        Button button;
+        TextView textViewDev;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            button = FindViewById<Button>(Resource.Id.MyButton);
+            textViewDev = FindViewById<TextView>(Resource.Id.textViewDev);
+            
+            button.Click += Button_Click; ;
+        }
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            textViewDev.Text = "Califiqueme ya acabe miss";
+            button.Text = "Gracias Cambiaste el texto";
+            
+        }
+    }
+}
+
